dcf9c836f44dcf00eac69e3645d73c705d1d066e
Alisa Fusha <alisa.fusha@student.lab.fi>
2025-04-09 11:03:15 +0300

    valmis laskintehtävä

--- scripts/laskin.js
+++ scripts/laskin.js
@@ -1,11 +1,13 @@
-function init(){
-    let nro1 = Math.floor(Math.random()* 10)+1;
+var counter = 0;
+
+function init() {
+    let nro1 = Math.floor(Math.random() * 10) + 1;
     document.getElementById('firstTextInput').value = nro1;
-    let nro2 = Math.floor(Math.random()* 10)+1;
+    let nro2 = Math.floor(Math.random() * 10) + 1;
     document.getElementById('firstTextInput1').value = nro2;
 }
 
-function minusNro1() { 
+function minusNro1() {
     let nro1 = parseInt(document.getElementById('firstTextInput').value);
     if (nro1 > 1) document.getElementById('firstTextInput').value = nro1 - 1;
 }
@@ -15,7 +17,7 @@ function plusNro1() {
     if (nro1 < 10) document.getElementById('firstTextInput').value = nro1 + 1;
 }
 
-function minusNro2() { 
+function minusNro2() {
     let nro2 = parseInt(document.getElementById('firstTextInput1').value);
     if (nro2 > 1) document.getElementById('firstTextInput1').value = nro2 - 1;
 }
@@ -30,5 +32,50 @@ function calculate() {
     let nro2 = parseInt(document.getElementById('firstTextInput1').value);
     let operation = document.getElementById('operation').value;
     let tulos = eval(nro1 + operation + nro2);
+    counter = counter + 1;
     document.getElementById('tulos').innerText = tulos;
+    document.getElementById('clicks').innerHTML = "Laskutoimituksia: " + counter + " kpl";
+
+    let element = document.querySelector('#element');
+    let p = document.createElement('p');
+    p.textContent = nro1 + operation + nro2 + "=" + tulos;
+    element.append(p);
+
+    /*Voi käyttää eval, switvh - else tai if - else rakennetta
+    /*switch(operation){
+    case '+':
+        tulos = nro1 + nro2;
+        document.getElementById('tulos').innerText = tulos;
+        break;
+    case '-':
+        tulos = nro1 - nro2;
+        document.getElementById('tulos').innerText = tulos;
+        break;
+    case '*':
+        tulos = nro1 * nro2;
+        document.getElementById('tulos').innerText = tulos;
+        break;
+    case '/':
+        tulos = nro1 / nro2;
+        document.getElementById('tulos').innerText = tulos.toFixed(2);
+        break;
+    default:
+        tulos = 'Virheellinen operaatio';
+        document.getElementById('tulos').innerText = tulos;
+    }*/
 }
+
+function iInfo() {
+    let i = document.getElementById('i-text');
+    if (i.style.display === "none") {
+        i.style.display = "block";
+    }
+    else { i.style.display = "none"; }
+}
+
+
+
+
+let div = document.createElement('div');
+div.append("some text");
+console.log(div.textContent);
