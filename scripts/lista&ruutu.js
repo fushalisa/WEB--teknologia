2328695f3a286822b2935e85c2588e39f25753cf
Alisa Fusha <alisa.fusha@student.lab.fi>
2025-04-22 19:10:09 +0300

    lista & ruudukko tiedostot

---
+++ scripts/lista&ruutu.js
@@ -0,0 +1,9 @@
+function Lista() {
+    document.getElementById('image-container').classList.remove('grid-container');
+    document.getElementById('image-container').classList.add('list-container');
+}
+
+function Ruudukko() {
+    document.getElementById('image-container').classList.remove('list-container');
+    document.getElementById('image-container').classList.add('grid-container');
+}
