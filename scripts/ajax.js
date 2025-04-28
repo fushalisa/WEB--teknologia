6ce36a4f597a890ada5ffa60079f94253e8ce379
Alisa Fusha <alisa.fusha@student.lab.fi>
2025-04-28 19:34:19 +0300

    ajax- tehtävä

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -2,33 +2,31 @@ var counter = 0;
 
 function init() {
     document.getElementById("gif").style.display = "none";
-
 }
 
 function getData() {
     document.getElementById("gif").style.display = "block";
 
-
     fetch("https://api.chucknorris.io/jokes/random")
 
         .then(function (response) {
             //.then, palauttaa tässä Promisen (lupauksen), jota käytetään seuraavassa then-funktiossa. 
-            return response.json();
+            return response.json(); //huom. tässä response tulee JSON-muodossa
         })
         .then(function (result) {
 
             document.getElementById("text").innerHTML += "<br>" + "<i>" + result.value + "</i>";
             document.getElementById("gif").style.display = "none";
-
         })
         .catch(function (error) {
-            //Jos lupaus epäonnistuu, tämä toiminto suoritetaan
+            //Jos lupaus epäonnistuu (eli lupaus on hylätty), tämä toiminto suoritetaan
             console.log(error);
         });
 
     counter = counter + 1;
     document.getElementById('clicks').innerHTML = "Laskuri: " + counter;
 }
+
 function getData1(buttonId) {
 
     fetch("https://api.chucknorris.io/jokes/random")
@@ -82,7 +80,4 @@ function getData2(hId) {
             console.log(error);
         });
 }
-
-
-
 
